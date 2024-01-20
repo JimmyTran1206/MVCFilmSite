84c04425a22e4732c4282c912451499da07b809d
Johnelle Walker <johnellewalker@gmail.com>
2024-01-19 17:46:12 -0700

    Created Controllers package & requisite class

---
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -0,0 +1,31 @@
+package com.skilldistillery.film.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FilmController {
+//TODO Complete Controller
+	
+//	@Autowired
+	
+	
+	
+	// TODO : Implement a request handler for:
+		// path "GetStateData.do"
+		// params "name"
+		// method GET
+		// return : ModelAndView
+		// view : "WEB-INF/result.jsp"
+		// object : "state", StateDAO.getStateByName
+//	@RequestMapping
+	
+	// TODO : Implement a request handler for:
+		// path "GetStateData.do"
+		// params "abbr"
+		// method GET
+		// return : ModelAndView
+		// view : "WEB-INF/result.jsp"
+		// object : "state", StateDAO.getStateByAbbreviation
+//	@RequestMapping
+
+}
